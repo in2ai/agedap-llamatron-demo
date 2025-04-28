bc34717f161baa528d3e8b0b60cb749cdbd89374
Adrián Ortega Rodríguez <adrian>
2025-04-28 11:30:34 +0200

    test con electron

--- src/app/views/config/config.component.spec.ts
+++ src/app/views/config/config.component.spec.ts
@@ -15,6 +15,45 @@ describe('ConfigComponent', () => {
   let component: ConfigComponent;
   let fixture: ComponentFixture<ConfigComponent>;
 
+  beforeAll(() => {
+    (window as any).electronAPI = {
+      runNodeCode: (data: any) => {
+        switch (data.func) {
+          case 'test':
+            return Promise.resolve({ func: data.func, result: 'Función test ejecutada' });
+          case 'state':
+            const state = {
+              modelPath: 'C:\\path\\to\\model.bin',
+              configuration: `{ "batchSize": 128, "contextSize": 4096, "gpuLayers": 1, "maxTokens": 2048, "temperature": 0.7, "threads": 4, "topK": 40, "topP": 0.4 }`,
+            };
+            return Promise.resolve({
+              func: data.func,
+              ...state,
+            });
+          case 'selectModel':
+            const config = `{ "batchSize": 128, "contextSize": 4096, "gpuLayers": 1, "maxTokens": 2048, "temperature": 0.7, "threads": 4, "topK": 40, "topP": 0.4 }`;
+            let configuration: any = undefined;
+            if (config)
+              configuration = JSON.parse(config, (key: any, value: any) =>
+                value === null ? undefined : value
+              );
+            const loadModal = (configuration: any) => {
+              console.log('Loading model with configuration:', configuration);
+            };
+            loadModal(configuration);
+            return Promise.resolve({
+              func: data.func,
+              modelName: 'model.bin',
+              modelPath: 'C:\\path\\to\\model.bin',
+            });
+        }
+        return Promise.resolve({ func: data.func, result: 'fake result' });
+      },
+      onPartialMessageResponse: () => {},
+      onNewExternalMessage: () => {},
+    };
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -32,9 +71,26 @@ describe('ConfigComponent', () => {
     fixture = TestBed.createComponent(ConfigComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    await component.ngOnInit();
+    expect(component.electronTest).toBeTrue();
+    console.log('electronTest:', component.electronTest);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call selectModel and update modelName and modelLoaded', async () => {
+    await component.selectModel();
+    fixture.detectChanges();
+
+    console.log('Model name:', component.modelName);
+    console.log('Model loaded:', component.modelLoaded);
+    console.log('Is selecting model:', component.isSelectingModel);
+
+    expect(component.modelName).toBe('model.bin');
+    expect(component.modelLoaded).toBeTrue();
+    expect(component.isSelectingModel).toBeFalse();
   });
 });
